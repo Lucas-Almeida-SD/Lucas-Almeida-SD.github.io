a73726b975fd2196b7989ad7cf7c6c45bae8eb46
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-06 10:46:13 -0300

    refactor: criado map para renderizar minhas skills

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import skills from '../imgs/skills.svg';
+import myskills from '../helpers/mySkills';
 import Tilt from 'react-vanilla-tilt';
 import './skills.css';
 
@@ -14,60 +15,15 @@ function Skills() {
             <img src={ skills } alt="Skills" />
           </div>
           <div className="skills-div">
-            <Tilt>
+            {myskills.map((skill) => (
+              <Tilt key={skill.title}>
               <img
-                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" 
-                alt="VS Code"
+                src={skill.url}
+                alt={skill.title}
+                title={skill.title}
               />
             </Tilt>
-            <Tilt>
-              <img
-                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"
-                alt="Git"
-              />
-            </Tilt>
-            <Tilt>
-              <img
-                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
-                alt="GitHub"
-              />
-            </Tilt>
-            <Tilt>
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg"
-                alt="HTML"
-              />
-            </Tilt>
-            <Tilt>
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg"
-                alt="CSS"
-              />
-            </Tilt>
-            <Tilt>
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg"
-                alt="JavaScript"
-              />
-            </Tilt>
-            <Tilt>
-              <img
-                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg"
-                alt="Jest"
-              />
-            </Tilt>
-            <Tilt>
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg"
-                alt="React"
-              />
-            </Tilt>
-            <Tilt>
-              <img
-                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg"
-                alt="Redux"
-              />
-            </Tilt>
+            ))}
           </div>
         </section>
       </main>
